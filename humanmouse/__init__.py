662ce07f66c5614a2c2698db46efa646844edd5e
Mateusz Kielbasa <mofficial1234@gmail.com>
2025-07-07 17:30:34 +0200

    chore: remove keyboard-related stubs and tests from mouse-humanizer; mouse-only API

--- humanmouse/__init__.py
+++ humanmouse/__init__.py
@@ -1,14 +1,21 @@
 from .windows_controller import HumanMouseController
 from .linux_controller import LinuxHumanMouseController
 
-HumanMouse = LinuxHumanMouseController
-
-def type_text(text):
-    pass
-
-def press_key(key):
-    pass
-
-def hotkey(*args):
-    pass
+class HumanMouse:
+    def __init__(self, *args, **kwargs):
+        self._impl = LinuxHumanMouseController(*args, **kwargs)
+    def move(self, pos, duration=0.5):
+        return self._impl.move(pos, duration=duration)
+    def click(self, pos, button='left'):
+        return self._impl.click(pos, button=button)
+    def hover(self, pos):
+        return self._impl.hover(pos)
+    def scroll(self, amount):
+        return self._impl.scroll(amount)
+    @property
+    def screen_width(self):
+        return self._impl.screen_width
+    @property
+    def screen_height(self):
+        return self._impl.screen_height
 
--- tests/test_humanmouse.py
+++ tests/test_humanmouse.py
@@ -1,24 +1,18 @@
 import pytest
-from humanmouse import HumanMouse, type_text, press_key, hotkey
+from humanmouse import HumanMouse
 
 def test_mouse_move_and_click():
     mouse = HumanMouse()
+    # Move to a safe position (center of screen)
     screen_width, screen_height = mouse.screen_width, mouse.screen_height
     center = (screen_width // 2, screen_height // 2)
     mouse.move(center, duration=0.5)
     mouse.click(center)
+    # No assertion: this is a smoke test for errors
 
 def test_mouse_scroll():
     mouse = HumanMouse()
     mouse.scroll(5)
     mouse.scroll(-5)
-
-def test_type_text():
-    type_text("Hello, world!")
-
-def test_press_key():
-    press_key("enter")
-
-def test_hotkey():
-    hotkey("ctrl", "a")
+    # No assertion: just check for errors
 
